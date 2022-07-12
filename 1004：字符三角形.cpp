fafee5b26563a005fd48ab0edc97a9b0f48f2b47
jasonXCH <98110922+jasonXCH@users.noreply.github.com>
2022-07-12 23:38:33 +0800

    Add files via upload

---
+++ 1004：字符三角形.cpp
@@ -0,0 +1,18 @@
+/*
+  *
+ ***
+*****
+*/
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    char A;
+    cin >> A;
+    cout <<"  "<< A << endl;
+    cout << " " << A << A << A << endl;
+    cout << A << A << A << A << A << endl;
+
+    return 0;
+}
